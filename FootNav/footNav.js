a0220375c7756d999f03658af2d13fd3ed43c9d0
bolaalcatras <jhonalexander2016.com@gmail.com>
2024-10-13 01:34:05 -0500

    foot y nav terminados

--- FootNav/footNav.js
+++ FootNav/footNav.js
@@ -1,11 +1,33 @@
-function loadTemplate(templateId, filePath) {
+function loadTemplate(templateId, filePath, callback) {
     fetch(filePath)
         .then(response => response.text())
         .then(data => {
             document.getElementById(templateId).innerHTML = data;
+            if (callback) callback();  // Ejecuta el callback si está definido
         })
         .catch(error => console.error('Error al cargar la plantilla:', error));
 }
 
-loadTemplate('header-placeholder', '../../../FootNav/header.html');
+// Cargar el header y luego ejecutar el código JavaScript necesario
+loadTemplate('header-placeholder', '../../../FootNav/header.html', function() {
+    // Una vez cargado el header, ejecuta el JavaScript necesario
+    const servicios = document.querySelector('.servicios');
+    const submenu = document.querySelector('.submenu'); 
+    if (servicios) {  // Verificar si el elemento existe
+        servicios.addEventListener('click', () => {
+            const submenu = document.querySelector('.submenu');
+            submenu.classList.toggle('active');  // Alternar clase 'active' para mostrar u ocultar el menu
+        });
+        
+    }
+
+    // Escuchar clics en cualquier parte del documento
+    document.addEventListener('click', function(event) {
+        // Si el clic es fuera del menu y fuera del botón "Servicios", se oculta el menu
+        if (!servicios.contains(event.target) && !submenu.contains(event.target)) {
+            submenu.classList.remove('active');
+        }
+    });
+});
+
 loadTemplate('footer-placeholder', '../../../FootNav/footer.html');
