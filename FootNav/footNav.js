005181013a152242f249ab69f18a26e89d9a5950
Antonio Rodriguez <anttoniio2312@gmail.com>
2024-10-13 12:14:31 -0500

    cambios en footer y header

--- FootNav/footNav.js
+++ FootNav/footNav.js
@@ -3,27 +3,23 @@ function loadTemplate(templateId, filePath, callback) {
         .then(response => response.text())
         .then(data => {
             document.getElementById(templateId).innerHTML = data;
-            if (callback) callback();  // Ejecuta el callback si está definido
+            if (callback) callback();
         })
         .catch(error => console.error('Error al cargar la plantilla:', error));
 }
 
-// Cargar el header y luego ejecutar el código JavaScript necesario
 loadTemplate('header-placeholder', '../../../FootNav/header.html', function() {
-    // Una vez cargado el header, ejecuta el JavaScript necesario
     const servicios = document.querySelector('.servicios');
     const submenu = document.querySelector('.submenu'); 
-    if (servicios) {  // Verificar si el elemento existe
+    if (servicios) { 
         servicios.addEventListener('click', () => {
             const submenu = document.querySelector('.submenu');
-            submenu.classList.toggle('active');  // Alternar clase 'active' para mostrar u ocultar el menu
+            submenu.classList.toggle('active');
         });
         
     }
 
-    // Escuchar clics en cualquier parte del documento
     document.addEventListener('click', function(event) {
-        // Si el clic es fuera del menu y fuera del botón "Servicios", se oculta el menu
         if (!servicios.contains(event.target) && !submenu.contains(event.target)) {
             submenu.classList.remove('active');
         }
