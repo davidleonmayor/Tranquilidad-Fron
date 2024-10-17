092453985ba274fba4bf90ada39c165061d73315
Dayanna Gonzalez <derlygonzalez688@gmail.com>
2024-10-17 17:10:02 -0500

    Descripción breve de los cambios

---
+++ musicoterapia/Vistas1.1/footNav.js
@@ -0,0 +1,29 @@
+function loadTemplate(templateId, filePath, callback) {
+    fetch(filePath)
+        .then(response => response.text())
+        .then(data => {
+            document.getElementById(templateId).innerHTML = data;
+            if (callback) callback();
+        })
+        .catch(error => console.error('Error al cargar la plantilla:', error));
+}
+
+loadTemplate('header-placeholder', '../../../FootNav/header.html', function() {
+    const servicios = document.querySelector('.servicios');
+    const submenu = document.querySelector('.submenu'); 
+    if (servicios) { 
+        servicios.addEventListener('click', () => {
+            const submenu = document.querySelector('.submenu');
+            submenu.classList.toggle('active');
+        });
+        
+    }
+
+    document.addEventListener('click', function(event) {
+        if (!servicios.contains(event.target) && !submenu.contains(event.target)) {
+            submenu.classList.remove('active');
+        }
+    });
+});
+
+loadTemplate('footer-placeholder', '../../../FootNav/footer.html');
