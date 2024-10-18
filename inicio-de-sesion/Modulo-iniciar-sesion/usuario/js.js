751aa41d17d1c9462130e28bcdd96ca992a88505
balvin22 <sb1181358@gmail.com>
2024-10-17 20:03:29 -0500

    otros-cambios

--- inicio-de-sesion/Modulo-iniciar-sesion/usuario/js.js
+++ inicio-de-sesion/Modulo-iniciar-sesion/usuario/js.js
@@ -1,13 +1,31 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const servicios = document.getElementById('serviciosIcon');
-    const Submenu = document.getElementById('serviciosSubmenu');
-    const Usuario = document.getElementById('userIcon');
-    const Menu = document.getElementById('menu-icon');
-    const Usuariosubmenu = document.getElementById('userSubmenu');
+    // const servicios = document.getElementById('serviciosIcon');
+    // const Submenu = document.getElementById('serviciosSubmenu');
+    // const Usuario = document.getElementById('userIcon');
+    // const Menu = document.getElementById('menu-icon');
+    // const Usuariosubmenu = document.getElementById('userSubmenu');
     const perfil = document.getElementById('profilePic');
     const perfilfoto = document.getElementById('profilePic1');
     const modal = document.getElementById('modal');
     const closeModal = document.querySelector('.close');
+    
+    // Mostrar el modal al hacer clic en la imagen de perfil
+    perfil.addEventListener('click', function() {
+        modal.style.display = 'flex'; // Mostrar modal
+    });
+    perfilfoto.addEventListener('click', function() {
+        modal.style.display = 'flex'; // Mostrar modal
+    });
+    // Cerrar modal al hacer clic en la "X"
+    closeModal.addEventListener('click', function() {
+        modal.style.display = 'none'; // Cerrar modal al hacer clic en la 'X'
+    });
+    window.addEventListener('click', function(event) {
+        if (event.target === modal) {
+            modal.style.display = 'none';
+        }
+    });
+    });
     
     // Mostrar el formulario de edición cuando se haga clic en el botón "Editar"
     const editar= document.getElementById('editarPerfilBtn');
@@ -27,43 +45,31 @@ document.addEventListener('DOMContentLoaded', () => {
         }
     });
 
-    // Funcionalidad para los submenús de "Servicios" y "Usuario"
-    servicios.addEventListener('click', (event) => {
-        event.preventDefault(); // Evitar que el enlace recargue la página
-        Submenu.style.display = Submenu.style.display === 'block' ? 'none' : 'block';
-    });
+    // // Funcionalidad para los submenús de "Servicios" y "Usuario"
+    // servicios.addEventListener('click', (event) => {
+    //     event.preventDefault(); // Evitar que el enlace recargue la página
+    //     Submenu.style.display = Submenu.style.display === 'block' ? 'none' : 'block';
+    // });
 
-    Usuario.addEventListener('click', (event) => {
-        event.preventDefault(); // Evitar que el enlace recargue la página
-        Usuariosubmenu.style.display = Usuariosubmenu.style.display === 'block' ? 'none' : 'block';
-    });
-    Menu.addEventListener('click', (event) => {
-        event.preventDefault(); // Evitar que el enlace recargue la página
-        Usuariosubmenu.style.display = Usuariosubmenu.style.display === 'block' ? 'none' : 'block';
-    });
+    // Usuario.addEventListener('click', (event) => {
+    //     event.preventDefault(); // Evitar que el enlace recargue la página
+    //     Usuariosubmenu.style.display = Usuariosubmenu.style.display === 'block' ? 'none' : 'block';
+    // });
+    // Menu.addEventListener('click', (event) => {
+    //     event.preventDefault(); // Evitar que el enlace recargue la página
+    //     Usuariosubmenu.style.display = Usuariosubmenu.style.display === 'block' ? 'none' : 'block';
+    // });
 
     // Ocultar submenús al hacer clic fuera de ellos
-    window.addEventListener('click', (event) => {
-        if (!servicios.contains(event.target)) {
-            Submenu.style.display = 'none';
-        }
-        if (!Usuario.contains(event.target)) {
-            Usuariosubmenu.style.display = 'none';
-        }
+    // window.addEventListener('click', (event) => {
+    //     if (!servicios.contains(event.target)) {
+    //         Submenu.style.display = 'none';
+    //     }
+    //     if (!Usuario.contains(event.target)) {
+    //         Usuariosubmenu.style.display = 'none';
+    //     }
       
-    });
+    // });
 
-    // Mostrar el modal al hacer clic en la imagen de perfil
-    perfil.addEventListener('click', function() {
-        modal.style.display = 'flex'; // Mostrar modal
-    });
 
-    perfilfoto.addEventListener('click', function() {
-        modal.style.display = 'flex'; // Mostrar modal
-    });
 
-    // Cerrar modal al hacer clic en la "X"
-    closeModal.addEventListener('click', function() {
-        modal.style.display = 'none'; // Cerrar modal al hacer clic en la 'X'
-    });
-});
