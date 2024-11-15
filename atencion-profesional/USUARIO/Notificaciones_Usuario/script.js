148607440cafc20a349df3208db16e16b24e7596
Pinedita777 <pinedajuandavid428@gmail.com>
2024-11-14 20:03:16 -0500

    Cambios generales

--- atencion-profesional/USUARIO/Notificaciones_Usuario/script.js
+++
@@ -1,7 +0,0 @@
-const menuIcon = document.querySelector('.menu-icon');
-  const navbar = document.querySelector('header');
-
-  // Añadir un event listener para abrir/cerrar el menú
-  menuIcon.addEventListener('click', () => {
-    navbar.classList.toggle('menu-open');
-  });
